d7a70b2b261c266e6a813b8454bce909b8eb76ed
Craxy <16610792+CraxyTM@users.noreply.github.com>
2019-12-03 19:15:04 +0100

    add temperature reader add correct response for temperature requests add temperature units add json support

--- src/main/java/de/craxy/pitemp/Main.java
+++ src/main/java/de/craxy/pitemp/Main.java
@@ -1,5 +1,9 @@
 package de.craxy.pitemp;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
+
 /**
  * Main class for PiTemp.
  *
@@ -14,12 +18,20 @@ public class Main {
      */
     public static void main(String[] args) {
         ArgsParser parser = new ArgsParser(args);
+
+        if (parser.hasArgument("debug") && parser.getStringValue("debug").get().equalsIgnoreCase("true")) {
+            LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+            ch.qos.logback.classic.Logger rootLogger = loggerContext.getLogger("de.craxy.pitemp");
+            rootLogger.setLevel(Level.DEBUG);
+        }
+
         new PiTemp(parser.getIntValue("port").orElse(80),
                 parser.getBooleanValue("ssl").orElse(false),
                 parser.getIntValue("sslport").orElse(443),
                 parser.getStringValue("context").orElse("/"),
                 parser.getStringValue("sslcertlocation").orElse(null),
-                parser.getStringValue("keystorepassword").orElse(null));
+                parser.getStringValue("keystorepassword").orElse(null),
+                parser.getStringValue("temperaturefilepath").orElse(null));
     }
 
 }
--- src/main/java/de/craxy/pitemp/handler/TemperatureHandler.java
+++ src/main/java/de/craxy/pitemp/handler/TemperatureHandler.java
@@ -1,22 +1,31 @@
 package de.craxy.pitemp.handler;
 
+import de.craxy.pitemp.temperature.TemperatureReader;
+import de.craxy.pitemp.temperature.TemperatureUnit;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A @{@link Handler} that responds to all requests regarding the temperature reading.
  */
 public class TemperatureHandler implements Handler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TemperatureHandler.class);
+    private TemperatureReader temperatureReader;
+
+    /**
+     * Creates a new temperature handler.
+     *
+     * @param temperatureReader a temperature reader that provides the current system temperature.
+     */
+    public TemperatureHandler(TemperatureReader temperatureReader) {
+        this.temperatureReader = temperatureReader;
+    }
 
     @Override
     public void handle(@NotNull Context ctx) {
         ctx.status(200);
-        ctx.result("Working");
-        LOGGER.info("Connection from: " + ctx.ip());
+        //TODO: parameter for temperature unit
+        ctx.json(new TemperatureResponse(temperatureReader.getLatestTemp(TemperatureUnit.CELSIUS), TemperatureUnit.CELSIUS));
     }
 }
